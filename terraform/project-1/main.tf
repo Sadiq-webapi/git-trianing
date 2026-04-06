7104d6cec0cb60dfffa38776dcf9aaad94655f91
Sadiq-webapi <mohamedsadiq9741@gmail.com>
2026-04-06 20:23:45 +0530

    Update main.tf

--- terraform/project-1/main.tf
+++ terraform/project-1/main.tf
@@ -10,8 +10,6 @@ terraform {
 
 provider "aws" {
   region     = "ap-south-2"
-  access_key = "YOUR_AWS_ACCESS_KEY_ID"
-  secret_key = "YOUR_AWS_SECRET_ACCESS_KEY"
 }
 
 resource "aws_instance" "my-aws_instance" {
