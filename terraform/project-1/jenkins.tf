80ad5f141ec9d1788b29cf433611181ca0678466
sadiq <sadiq@9741>
2026-04-07 16:41:38 +0530

    added new files

---
+++ terraform/project-1/jenkins.tf
@@ -0,0 +1,46 @@
+pipeline {
+    agent any
+
+    environment {
+        AWS_DEFAULT_REGION = "ap-south-2"
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Sadiq-webapi/git-trianing.git'
+            }
+        }
+
+        stage('Terraform Init') {
+            steps {
+                sh 'terraform init'
+            }
+        }
+
+        stage('Terraform Validate') {
+            steps {
+                sh 'terraform validate'
+            }
+        }
+
+        stage('Terraform Plan') {
+            steps {
+                sh 'terraform plan -out=tfplan'
+            }
+        }
+
+        stage('Terraform Apply') {
+            steps {
+                input message: "Apply Terraform changes?"
+                sh 'terraform apply -auto-approve tfplan'
+            }
+        }
+    }
+
+    post {
+        always {
+            archiveArtifacts artifacts: '**/terraform.tfstate', fingerprint: true
+        }
+    }
+}
