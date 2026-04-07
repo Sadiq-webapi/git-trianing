7ac21b8f58217dd3ed4681acaa7d6b1b8ba7433a
Sadiq-webapi <mohamedsadiq9741@gmail.com>
2026-04-07 17:10:12 +0530

    Update jenkins.tf

--- terraform/project-1/jenkins.tf
+++ terraform/project-1/jenkins.tf
@@ -1,46 +1,39 @@
 pipeline {
     agent any
-
-    environment {
-        AWS_DEFAULT_REGION = "ap-south-2"
-    }
-
     stages {
         stage('Checkout') {
             steps {
                 git branch: 'master', url: 'https://github.com/Sadiq-webapi/git-trianing.git'
             }
         }
-
         stage('Terraform Init') {
             steps {
-                sh 'terraform init'
+                dir('terraform/project-1') {
+                    sh 'terraform init'
+                }
             }
         }
-
         stage('Terraform Validate') {
             steps {
-                sh 'terraform validate'
+                dir('terraform/project-1') {
+                    sh 'terraform validate'
+                }
             }
         }
-
         stage('Terraform Plan') {
             steps {
-                sh 'terraform plan -out=tfplan'
+                dir('terraform/project-1') {
+                    sh 'terraform plan -out=tfplan'
+                }
             }
         }
-
         stage('Terraform Apply') {
             steps {
                 input message: "Apply Terraform changes?"
-                sh 'terraform apply -auto-approve tfplan'
+                dir('terraform/project-1') {
+                    sh 'terraform apply -auto-approve tfplan'
+                }
             }
-        }
-    }
-
-    post {
-        always {
-            archiveArtifacts artifacts: '**/terraform.tfstate', fingerprint: true
         }
     }
 }
