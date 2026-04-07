274531910f05f4f88525312614b7f1676b061aec
Sadiq-webapi <mohamedsadiq9741@gmail.com>
2026-04-07 17:24:20 +0530

    Update s3.tf

--- terraform/project-1/s3.tf
+++ terraform/project-1/s3.tf
@@ -1,13 +1,3 @@
-terraform {
-    required_version="1.14.8"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.39.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "ap-south-2"
 }
