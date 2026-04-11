b5cab0db042d7307b57d89e3416351408db3fe32
Sadiq-webapi <mohamedsadiq9741@gmail.com>
2026-04-11 12:24:56 +0530

    Rename EKS cluster from 'practice-cluster' to 'clusters'

--- terraform/eks/eks.tf
+++ terraform/eks/eks.tf
@@ -21,7 +21,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "practice-cluster"
+  cluster_name    = "clusters"
   cluster_version = "1.29"
 
   subnet_ids = module.vpc.private_subnets
